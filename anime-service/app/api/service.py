09d4d0d33dc96945e34ec1240dbe3650e3cfe3ae
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-07-15 20:57:01 +0530

    docker-compose

--- anime-service/app/api/service.py
+++ anime-service/app/api/service.py
@@ -1,7 +1,7 @@
 import os
 import httpx
 
-STUDIO_SERVICE_HOST_URL = "https://localhost:8002/api/v1/studio/"
+STUDIO_SERVICE_HOST_URL = "http://localhost:8002/api/v1/studio/"
 url = os.environ.get('STUDIO_SERVICE_HOST_URL') or STUDIO_SERVICE_HOST_URL
 
 def is_studio_present(studio_id: int):
